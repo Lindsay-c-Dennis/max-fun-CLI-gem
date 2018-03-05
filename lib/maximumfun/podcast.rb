4c1b34ff9a567dabc16abc9d2495c77f1e45cbdb
Lindsay <lindsay.c.dennis@gmail.com>
2018-03-05 16:48:58 -0600

    tinkering with scraper

--- lib/maximumfun/podcast.rb
+++ lib/maximumfun/podcast.rb
@@ -1,7 +1,11 @@
 class Podcast
   attr_accessor :name, :host, :latest_episode_title, :description, :latest_episode_description, :show_page_url 
-  @@all = []
+    @@all = []
 
   def self.new_from_page()
   end
+
+  def self.all
+  	@@all 
+  end	
 end
--- lib/maximumfun/scraper.rb
+++ lib/maximumfun/scraper.rb
@@ -1,11 +1,28 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
 class Scraper
   
-  def get_page
-  	Nokogiri::HTML(open("http://www.maximumfun.org/shows/comedy"))
+  def self.scrape_list_page(index_url)
+  	list_page = Nokogiri::HTML(open(index_url))
+  	podcasts = []
+  	list_page.css(".view-content").each do |show_summary|
+  		show_summary.css(".show-summary-details").each do |podcast|
+  			podcast_name = podcast.css("a").text
+  			podcast_host = podcast.css("p.submitted").text
+  		    podcast_description = podcast.css("p")[2]
+  		    podcast_url = "#{podcast.attr('href')}"
+  		    podcasts << {title: podcast_name, host: podcast_host, description: podcast_description, show_page_url: podcast_url}
+  		end
+  	end
+    puts podcasts[0]
+  	
   end
 
-  def scrape_podcast_list
-  	self.get_page.css()
-  end
+
+
+Scraper.scrape_list_page("http://www.maximumfun.org/shows/comedy")
+
 
 end
